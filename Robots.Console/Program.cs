ff6b0fd9e336822ed8b6adc74de2eafd902431ce
charlesremy <charles.remy@my-digital-school.org>
2023-12-12 13:15:55 +0100

    cours

--- Robots.Console/Program.cs
+++ Robots.Console/Program.cs
@@ -4,10 +4,10 @@ using Robots;
 Console.WriteLine("Hello, World!");
 
 
-Robot robot = new Robot("R2D2" Point);
+Robot robot = new Robot("R2D2", 0, 0);
 Console.WriteLine(robot.AfficherPosition());
 
-RobotMobile robotMobile = new RobotMobile("R2D2", Point Positon, 10) ;
+RobotMobile robotMobile = new RobotMobile("R2D2", 0, 0, 10);
 robotMobile.Avancer(10, 10);
 Console.WriteLine($"ma vitesse est de {robotMobile.CalculerDuréeDeDéplacement(20, 20)}");
 Console.WriteLine(robotMobile.AfficherPosition());
--- Robots/IRobotNettoyage.cs
+++ Robots/IRobotNettoyage.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Robots
 {
     public interface IRobotNettoyage
     {
-        public void Nettoyer();
-    };
+        void Nettoyer();
+    }
 }
-
