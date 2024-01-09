34ff0e6c0a7e95975805b96b8d4b8e1d97a189c6
charlesremy <charles.remy@my-digital-school.org>
2024-01-09 08:40:55 +0100

    Troisième jour

---
+++ Robots/NewRobot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Robots
+{
+	public class NewRobot
+	{
+        public NewRobot()
+        {
+            CreationDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Robot {Id} : {Name} - {Status} - {CreationDate}";
+        }
+    }
+}
+
